1d66ec8c0afa8750aac03b4611b5fefbae823b7d
tania <natania.william09@gmail.com>
2024-07-20 11:42:09 +0700

    Completed project and fixed colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,tjx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,tjx,tsx}", "./fake-server/**/*.{json}",],
   theme: {
     extend: {
       screens: {
@@ -10,6 +10,20 @@ module.exports = {
       }
     },
   },
+  safelist: [
+    'bg-[#4ca735]',
+    'bg-[#3d8a2d]',
+    'bg-[#4eabd2]',
+    'bg-[#4091af]',
+    'bg-[#db3e40]',
+    'bg-[#a13434]',
+    'bg-[#88398a]',
+    'bg-[#762e69]',
+    'bg-[#8fca77]',
+    'bg-[#8bd0e4]',
+    'bg-[#e9848a]',
+    'bg-[#ca8ec5]'
+  ],
   plugins: [
     require('@tailwindcss/typography'),
   ],
